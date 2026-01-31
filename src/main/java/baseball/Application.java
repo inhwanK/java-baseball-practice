3df6f5d2fd90414433be80b0fd7c33dcc1e70c76
inhwanK <inhan1009@naver.com>
2026-01-31 22:46:01 +0900

    feat : 게임 시작을 위한 세팅로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.manage.GameFinisher;
+import baseball.manage.GameManager;
+import baseball.manage.GameProcessor;
+import baseball.manage.GameStarter;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 게임 매니저에게 게임 시작 요청 메시지 전송
+        GameManager gameManager = new GameManager(new GameStarter(), new GameProcessor(), new GameFinisher());
+        gameManager.start();
     }
 }
